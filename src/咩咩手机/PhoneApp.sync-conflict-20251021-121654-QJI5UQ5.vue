ff2fd8715b4315ec9c5eb389c890e0eb37603750
Mooooooon <maoxianmoon@gmail.com>
2025-10-21 20:18:34 +0800

    调整文件权限以确保一致性

---
+++ src/咩咩手机/PhoneApp.sync-conflict-20251021-121654-QJI5UQ5.vue
@@ -0,0 +1,18 @@
+<template>
+  <PhoneFrame>
+    <router-view />
+  </PhoneFrame>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import PhoneFrame from './components/PhoneFrame.vue';
+
+onMounted(() => {
+  console.log('手机UI已加载');
+});
+</script>
+
+<style lang="scss">
+// 全局样式将在此定义
+</style>
